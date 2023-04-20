2b66b2ad070564674288c85b5b8ca573bb8acbb5
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-04-20 17:53:19 +0530

    Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+
 
 export const metadata = {
   title: "Clock",
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
